5e3323930cf598e42e8bd3b79d04ded64b8c2c74
ASHISH KUMAR BHOI <33381078+AshishBhoi@users.noreply.github.com>
2019-10-16 06:19:31 +0530

    Code Cleaned Up

--- FirstProject/settings.py
+++ FirstProject/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '4s24e%%85+f&$)1748&@*ibvgmy!7k2s)lro)fb9hye^*zf2lb'
 DEBUG = True
 
 ALLOWED_HOSTS = ["app.ashishbhoi.com", "ubuntu.ashishbhoi.com"]
-
 
 # Application definition
 
@@ -52,24 +50,18 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'FirstProject.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
+    dict(BACKEND='django.template.backends.django.DjangoTemplates', DIRS=[os.path.join(BASE_DIR, 'templates')],
+         APP_DIRS=True, OPTIONS={
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-        },
-    },
+        }),
 ]
 
 WSGI_APPLICATION = 'FirstProject.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
@@ -80,7 +72,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,7 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -114,10 +104,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#SECURE_SSL_REDIRECT = True
-
