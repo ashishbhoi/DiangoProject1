58487fdf5318200bfdc5956e21dfe737b5c81f92
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2019-10-22 01:03:37 +0530

    Now new User can login logout and can view their  profile

--- FirstProject/urls.py
+++ FirstProject/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
 from users import views as user_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('register/', user_view.register, name='register'),
+    path('profile/', user_view.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
