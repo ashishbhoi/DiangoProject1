d4027d4e49dfc67eb704e6622cf241b4564a5e95
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2019-10-22 23:20:46 +0530

    Config For Google Cloud Storage Added

--- users/models.py
+++ users/models.py
@@ -2,8 +2,6 @@ import os
 from django.db import models
 from django.contrib.auth.models import User
 from google.oauth2 import service_account
-from PIL import Image
-import requests
 
 
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file('users/google-storage-api.json')
@@ -16,11 +14,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-    def save(self, **kwargs):
-        super().save()
-        img = Image.open(requests.get(self.image.url, stream=True).raw)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.name)
